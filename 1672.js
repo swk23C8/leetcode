9ac5f3b22e69fbdd0100f2ea3f83d7cd51aa7bee
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-12-27 17:59:40 +0900

    progress

--- 1672.js
+++ 1672.js
@@ -2,8 +2,19 @@
  * @param {number[][]} accounts
  * @return {number}
  */
- var maximumWealth = function(accounts) {
-    
+var maximumWealth = function (accounts) {
+    let max = 0;
+    let tmp = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        for (let j = 0; j < accounts[i].length; j++) {
+            tmp += accounts[i][j];
+        }
+        if (tmp > max) {
+            max = tmp;
+        }
+        tmp = 0;
+    }
+    return max;
 };
 
 // You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
