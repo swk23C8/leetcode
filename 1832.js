d4a43ff755f3199b7f5855fc1599f6b0f5606326
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-12-30 05:43:38 +0900

    progress

---
+++ 1832.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+ var checkIfPangram = function(sentence) {
+    
+};
+
+
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// Given a string sentence containing only lowercase English letters, 
+// return true if sentence is a pangram, or false otherwise.
+
+
+// Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+// Output: true
+// Explanation: sentence contains at least one of every letter of the English alphabet.
