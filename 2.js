5f603e992abd468b0c84f0170acbfd6cd2912b0d
swk23c8 <swk23c8@riseup.net>
2022-02-11 23:51:16 +0900

    grind

---
+++ 2.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+};
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
---
+++ 771.js
@@ -0,0 +1,4 @@
+const numJewelsInStones = (J, S) => {
+	const jewels = new Set(J)
+	return S.split('').reduce((res, s) => res + jewels.has(s), 0)
+};
