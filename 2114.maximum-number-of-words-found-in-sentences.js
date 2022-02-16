21ce42b40b9dcabc6d93f66f54826285b15b49ab
swk23c8 <swk23c8@riseup.net>
2022-02-17 00:59:11 +0900

    grind

---
+++ 2114.maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=2114 lang=javascript
+ *
+ * [2114] Maximum Number of Words Found in Sentences
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function (sentences) {
+	for ()
+};
+// @lc code=end
+
