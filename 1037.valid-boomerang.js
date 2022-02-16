e09f541ea84d42b4f4ecb560193d173d7b227779
swk23c8 <swk23c8@riseup.net>
2022-02-17 00:59:20 +0900

    grind

---
+++ 1037.valid-boomerang.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1037 lang=javascript
+ *
+ * [1037] Valid Boomerang
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function (points) {
+	console.log(points);
+};
+// @lc code=end
+
