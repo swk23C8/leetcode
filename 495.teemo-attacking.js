4b8ed356f269568f299cfbc9a4f6d8e4525197df
swk23c8 <swk23c8@riseup.net>
2022-03-14 01:34:09 +0900

    grind

---
+++ 495.teemo-attacking.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=495 lang=javascript
+ *
+ * [495] Teemo Attacking
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+    
+};
+// @lc code=end
+
